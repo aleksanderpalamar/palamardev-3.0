75e08a46d9cc59c77819f802b222e7970dc1f43d
aleksanderpalamar <apalamar@live.com>
2024-10-21 18:53:15 -0300

    Update .gitignore, refactor Page component, and modify Prisma DB URL.

--- app/(main)/blog/[slug]/page.tsx
+++ app/(main)/blog/[slug]/page.tsx
@@ -1,29 +1,16 @@
 import { getSinglePost } from "@/Functions/get-posts";
-import { Loader2 } from "lucide-react";
 import { marked } from "marked";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const post = await getSinglePost(params.slug)
-
-  const renderer = new marked.Renderer();
-
-  marked.setOptions({
-    renderer,
-    breaks: true,
-    gfm: true,
-  });
-
-  switch (post) {
-    case null:
-      return <div className="flex justify-center">Post not found</div>
-    case undefined:
-      return <div className="flex justify-center"><Loader2 className="w-6 h-6 animate-spin" /></div>
-  }
+  const content = marked.parse(post?.content || '')
 
   return (
     <div className="max-w-6xl mx-auto space-y-6">
-      <h1 className="text-4xl font-bold">{post.title}</h1>
-      <div 
+      <h1 className="text-4xl font-bold">
+        {post?.title}
+      </h1>
+      <div
         className="prose prose-lg max-w-none mx-auto dark:prose-invert
                    prose-headings:font-bold prose-headings:text-gray-900 dark:prose-headings:text-gray-100
                    prose-p:text-gray-700 dark:prose-p:text-gray-300
@@ -36,7 +23,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
                    prose-blockquote:text-gray-900 dark:prose-blockquote:text-gray-100
                    prose-img:rounded-xl prose-img:shadow-lg prose-img:shadow-gray-800/50
                    prose-img:object-cover prose-img:object-center space-y-4"
-        dangerouslySetInnerHTML={{ __html: marked.parse(post.content) }}
+        dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
   )
