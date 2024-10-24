56fcde2c3d44f1754e60b915314ac99883efaf8e
aleksanderpalamar <apalamar@live.com>
2024-10-24 15:09:51 -0300

    Add "/register" route to config matcher array.

--- middleware.ts
+++ middleware.ts
@@ -13,5 +13,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard", "/dashboard/admin/:path*"],
+  matcher: ["/dashboard", "/dashboard/admin/:path*", "/register"],
 }
